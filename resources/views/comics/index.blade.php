89c29a5676303f45f96d9d009534a7819cdcb3e1
Francesco Moles <francesco.moles@outlook.it>
2022-05-26 21:06:26 +0200

    destroy controller done

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,7 +4,12 @@
         {{ $comic->title }}
         {{ $comic->price }}
         <a href="{{ route('comics.show', $comic->id) }}">Detail</a>
-        <a href="{{ route('comics.edit', $comic->id) }}">Update</a><br>
+        <a href="{{ route('comics.edit', $comic->id) }}">Update</a>
+        <form action="{{ route('comics.destroy', $comic->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="Delete">
+        </form><br>
     @endforeach
     <br><a href="{{ route('comics.create') }}">New</a><br>
 @endsection
